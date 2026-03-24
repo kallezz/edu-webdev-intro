757b0d231b9a7326706803abcd1ab7eb252f57f6
kallezz <kalle.melender@hotmail.com>
2026-03-24 19:31:39 +0200

    Add contact page, add all styles, fix responsiveness, add mobile nav, etc.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Happy developing ✨')
+const menuToggle = document.getElementById('menu-toggle');
+const mainNav = document.getElementById('main-nav');
+
+menuToggle?.addEventListener('click', () => {
+    mainNav.classList.toggle('active');
+});
+
+// Close menu when a link is clicked
+mainNav?.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', () => {
+        mainNav.classList.remove('active');
+    });
+});
